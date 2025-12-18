91792e0c3b31ad67ae317fcf5560670c10407d89
nisra-explore <explore@nisra.gov.uk>
2025-12-18 12:29:15 +0000

    explore local stats url fixes

--- src/lib/ui/SearchResult.svelte
+++ src/lib/ui/SearchResult.svelte
@@ -35,7 +35,7 @@
 
         let search_code = postcode.replaceAll(" ", "");
 
-        fetch("https://raw.githubusercontent.com/NISRA-Tech-Lab/local-stats/main/search_data/CPD_LIGHT_JULY_2024.csv")
+        fetch("https://raw.githubusercontent.com/nisra-explore/local-stats/main/search_data/CPD_LIGHT_JULY_2024.csv")
             .then((response) => response.text())
             .then((data) => {
                 const rows = data.split("\n");
