beb16b2ecf95db8571fc5350540f6e9b216e142d
nisra-explore <explore@nisra.gov.uk>
2025-12-18 10:13:12 +0000

    Updates

--- _app/immutable/chunks/paths.050e00f8.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1e075iz)==null?void 0:s.base)??"/local-stats";var t;const e=((t=globalThis.__sveltekit_1e075iz)==null?void 0:t.assets)??"https://NISRA-Tech-Lab.github.io/local-stats";export{e as a,a as b};
