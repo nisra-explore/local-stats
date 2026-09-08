ca5d531286cab6fa3b5a1076ae5fa49bab9b52df
ConorOLoan <Conor.OLoan@nisra.gov.uk>
2026-09-08 22:50:06 +0100

    Format life expectancy to one decimal place

--- src/routes/[code]/+page.svelte
+++ src/routes/[code]/+page.svelte
@@ -1180,24 +1180,22 @@ function compareDensity (place) {
 				id: "lifeexpectancy",
 				year: pullYear("LE", data.place),
 				content: "<p>Male</p> <span class='text-big'>" + 
-					(check("LE.value.Males")).toLocaleString() +
-					"</span> years"+
-					"<p>Female</p> <span class='text-big'>" +
-					(check("LE.value.Females")).toLocaleString() + "</span> years",
+						(check("LE.value.Males")).toFixed(1) +
+						"</span> years"+
+						"<p>Female</p> <span class='text-big'>" +
+						(check("LE.value.Females")).toFixed(1) + "</span> years",
 				show: ["ni"]
 			},
-
 			box_3b: {
 				id: "lifeexpectancy",
 				year: pullYear("LE", data.place),
 				content: "<p>Male</p> <span class='text-big'>" + 
-					(check("LE.value.Males")).toLocaleString() +
-					"</span> years"+
-					"<span style='color: #1460aa'> (" + comp_short + " " + data.ni.data.LE.value.Males +") </span></p>"+
-						
-					"<p>Female</p> <span class='text-big'>" +
-					(check("LE.value.Females")).toLocaleString() + "</span> years"+
-					"<span style='color: #1460aa'> (" + comp_short + " " + data.ni.data.LE.value.Females +") </span></p>",
+						(check("LE.value.Males")).toFixed(1) +
+						"</span> years"+
+						"<span style='color: #1460aa'> (" + comp_short + " " + data.ni.data.LE.value.Males.toFixed(1) +") </span></p>"+
+						"<p>Female</p> <span class='text-big'>" +
+						(check("LE.value.Females")).toFixed(1) + "</span> years"+
+						"<span style='color: #1460aa'> (" + comp_short + " " + data.ni.data.LE.value.Females.toFixed(1) +") </span></p>",
 				show: [ "lgd", "dea"]
 			},
 			box_3c: {
